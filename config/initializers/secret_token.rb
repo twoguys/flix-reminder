20dd9db29f380d04cb71835d15f88c41cafc1225
Marshall Huss <mwhuss@gmail.com>
2011-08-19 15:22:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlixReminder::Application.config.secret_token = '3ab8e25536359b713b98aa70d989e1cdbec090bd97e91eb4d189fec6c0de3bd86861e1f156efa1659be230b86d64521bef0e88279a64c046a57e4e9692862acf'
