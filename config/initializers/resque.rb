2100efae1f68ced6ed75b78329dc0fee3d4ca2a0
David Browning <djbrowning@gmail.com>
2011-08-20 13:30:16 -0400

    Updated homepage styles and some general form styles.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#uri = URI.parse(ENV["REDISTOGO_URL"])
+#Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
