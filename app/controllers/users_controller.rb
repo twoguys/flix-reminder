b7c6e6c3cc9f2ef915c4fffd51b98747a9534340
Marshall Huss <mwhuss@gmail.com>
2011-08-29 23:14:39 -0400

    Fixing users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
   def create_or_login
-    #render text: request.env['omniauth.auth']
+    #render text: request.env['omniauth.auth'] and return
     omniauth = request.env["omniauth.auth"]
     user = User.find_or_create_by_netflix_id(omniauth["uid"])
     Rails.logger.info user.inspect
     if user.first_name.blank? || user.last_name.blank?
-      user.update_attributes(first_name: omniauth["user_hash"]["first_name"], last_name: omniauth["user_hash"]["last_name"])
+      user.update_attributes(first_name: omniauth["user_info"]["first_name"], last_name: omniauth["user_info"]["last_name"])
     end
     sign_in_and_redirect(:user, user)
   end
