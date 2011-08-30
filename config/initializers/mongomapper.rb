9ffa2077785e653bd5f07ec9a8239c428922ebd0
Marshall Huss <mwhuss@gmail.com>
2011-08-29 23:19:54 -0400

    Fixed mongo_mapper config

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,1 +1,11 @@
-MongoMapper.database = "flixreminder-#{Rails.env}"
+#MongoMapper.database = "flixreminder-#{Rails.env}"
+
+MongoMapper.config = { 
+  Rails.env => { 
+    'uri' => ENV['MONGOHQ_URL'] ||
+    "mongodb://localhost/flixreminder-#{Rails.env}"
+  }
+}
+
+MongoMapper.connect(Rails.env)
+
