f46f4005809cba24437953cb836e6bac175dac90
Marshall Huss <mwhuss@gmail.com>
2011-09-06 17:19:58 -0400

    Added user token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,24 @@ class User
   key :email,           String
   key :first_name,      String
   key :last_name,       String
+  key :token,           String
   key :day_of_the_week, Integer
   
   validates :email, presence: true, uniqueness: true
+  validates :token, presence: true, uniqueness: true
   validates :day_of_the_week, presence: true
   
+  before_create :create_token
+
   def name
     "#{first_name} #{last_name}"
+  end
+
+
+  private
+
+  def create_token
+    self.token = ActiveSupport::SecureRandom.hex(24)
   end
 
 end
