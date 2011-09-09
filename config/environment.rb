f3933be46c9b383a1475a771de30c7312cfa5265
Marshall Huss <mwhuss@gmail.com>
2011-09-09 08:23:01 -0400

    Added env.local.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,10 @@ require File.expand_path('../application', __FILE__)
 env_file = File.join(Rails.root, 'config', 'env.rb')
 load(env_file) if File.exists?(env_file)
 
+if Rails.env == "development"
+  local_env_file = File.join(Rails.root, 'config', 'env.local.rb')
+  load(local_env_file) if File.exists?(local_env_file)
+end
+
 # Initialize the rails application
 FlixReminder::Application.initialize!
